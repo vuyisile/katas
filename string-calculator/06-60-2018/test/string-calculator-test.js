dc15350847e453f306b6514ca63dff9f503ab013
vuyisile <vuyisilemark@gmail.com>
2018-06-11 12:11:06 +0200

    initial commit

---
+++ string-calculator/06-60-2018/test/string-calculator-test.js
@@ -0,0 +1,12 @@
+var chai = require('chai');
+var expect = chai.expect;
+var assert = require('assert');
+var stringCalculator = require('../string-calc');
+
+
+describe('test stringCalculator', function () {
+    it('', function () {
+        expect('').to.eql(0);
+    })
+
+})
